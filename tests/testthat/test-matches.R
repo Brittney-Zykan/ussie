1caad1226795ce381810bebdf446ac6aa467b298
Brittney-Zykan <Brittney_Zykan@mckinsey.com>
2022-07-26 14:28:45 -0400

    reset to btt22 2.3.2

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -1,24 +1,21 @@
 test_that("uss_make_matches works", {
-
-  # use the function
+  
+  local_warn_partial_match()
+  
+  # we have tested the behaviors of the validators elsewhere,
+  # here, we're just making sure the *right* error got thrown
+  expect_error(uss_make_matches(3, "foo"), class = "ussie_error_data")
+  expect_error(uss_make_matches(mtcars, "foo"), class = "ussie_error_cols")
+  
   italy <- uss_make_matches(engsoccerdata::italy, "Italy")
-
-  # check whether output is a tibble
+  
   expect_true(tibble::is_tibble(italy))
-
-  # check whether output has columns with the right names
-  expect_named(italy,
-               c("country", "tier", "season", "date", "home", "visitor",
-                  "goals_home", "goals_visitor"))
-
-  # check that the country column of the output is correct
+  expect_named(italy, cols_matches())
   expect_identical(unique(italy$country), "Italy")
 
-  # check that tier column is a factor
-  # it is important that tier is a factor (when you find a bug, add a test)
   expect_s3_class(italy$tier, "factor")
-
-  # setting new snapshot
+  
+  # not as robust as a full "identical" comparison
+  #  - still useful for column names, types, values for first few rows
   expect_snapshot(dplyr::glimpse(italy))
-
 })
